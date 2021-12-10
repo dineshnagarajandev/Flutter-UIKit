7b80f2fccd2d6d9c8383d0093a50f7d08c596895
Dinesh Nagarajan <contact@dineshnagarajan.dev>
2021-12-10 17:30:00 +0530

    Status screen UI update and included new class for create status screens

--- lib/WhatsApp/Status/WhatsAppStatus.dart
+++ lib/WhatsApp/Status/WhatsAppStatus.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/WhatsApp/chatConversation.dart';
+import 'package:my_app/WhatsApp/Status/WhatsAppStatusCreate.dart';
 
 class WhatsAppStatus extends StatefulWidget {
   const WhatsAppStatus({Key key}) : super(key: key);
@@ -12,7 +12,63 @@ class _WhatsAppStatusState extends State<WhatsAppStatus> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Status'),
+      body: ListView.separated(
+          separatorBuilder: (context, index) => Divider(),
+          itemBuilder: (context, index) {
+            print(index);
+            return Container(
+                color: Colors.grey[100],
+                height: 80,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30)),
+                      child: CircleAvatar(
+                        radius: 30.0,
+                        backgroundImage:
+                            NetworkImage('https://via.placeholder.com/150'),
+                        backgroundColor: Colors.lightBlueAccent,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 20,
+                        ),
+                        (index == 0)
+                            ? Text(
+                                "My status",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              )
+                            : Container(),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        (index == 0)
+                            ? Text(
+                                "Tap to add status update",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 16),
+                              )
+                            : Container(),
+                      ],
+                    ),
+                  ],
+                ));
+          },
+          itemCount: 1),
       floatingActionButton: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.end,
@@ -20,7 +76,13 @@ class _WhatsAppStatusState extends State<WhatsAppStatus> {
           FloatingActionButton(
             child: Icon(Icons.create, color: Colors.grey[800]),
             backgroundColor: Colors.grey[300],
-            onPressed: () {},
+            onPressed: () {
+              print("Create status tapped");
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => WhatsAppStatusCreate()));
+            },
           ),
           Divider(
             color: Colors.white,
@@ -28,7 +90,9 @@ class _WhatsAppStatusState extends State<WhatsAppStatus> {
           FloatingActionButton(
             child: Icon(Icons.camera_alt),
             backgroundColor: Colors.green,
-            onPressed: () {},
+            onPressed: () {
+              print("Camera tapped");
+            },
           ),
         ],
       ),
---
+++ lib/WhatsApp/Status/WhatsAppStatusCreate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WhatsAppStatusCreate extends StatefulWidget {
+  const WhatsAppStatusCreate({Key key}) : super(key: key);
+
+  @override
+  _WhatsAppStatusCreateState createState() => _WhatsAppStatusCreateState();
+}
+
+class _WhatsAppStatusCreateState extends State<WhatsAppStatusCreate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
