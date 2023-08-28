c76d5a4a425661df6d9f9b5c2004933603522687
Dinesh Nagarajan <dineshnagarajan19@gmail.com>
2023-08-28 21:42:22 +0530

    API url update

--- lib/ServiceManager/servicemanager.dart
+++ lib/ServiceManager/servicemanager.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 import 'package:http/http.dart' as http;
 
-const baseURL = 'https://jsonplaceholder.typicode.com';
+const baseURL = 'jsonplaceholder.typicode.com';
 
 class API {
   static Future getUser() {
-    var url = Uri.https(baseURL + '/users');
+    var url = Uri.https(baseURL, '/users');
     print(url);
     return http.get(url);
   }
